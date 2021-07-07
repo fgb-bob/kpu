d67fcd59c9ca882ab89d4a06abbb0b36c2059f6e
lee hayoung <leeha0507@fgb.ai>
2021-07-07 18:19:15 +0900

    #10 6일차

---
+++ lee-hayoung/react study/learn-redux-middleware/src/containers/PostListContainer.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PostList from '../components/PostList';
+import { getPosts } from '../modules/posts';
+
+function PostListContainer() {
+  const { data, loading, error } = useSelector(state => state.posts.posts);
+  const dispatch = useDispatch();
+
+  // 컴포넌트 마운트 후 포스트 목록 요청
+  useEffect(() => {
+    //if (data) return; //이거하면 데이터가 이미 있으면 다시 안불러온다. (최신데이터는 x)
+    dispatch(getPosts());
+  }, [data, dispatch]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
+  return <PostList posts={data} />;
+}
+
+export default PostListContainer;
